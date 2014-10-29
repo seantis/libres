4ca61078a4756e2eb9ca573eaa00c7fc00279bef
Denis Krienbühl <denis@href.ch>
2014-10-29 17:19:50 +0100

    Reactivate second scheduler test

--- libres/tests/test_scheduler.py
+++ libres/tests/test_scheduler.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from libres import new_scheduler
 
 
 def test_managed_allocations(scheduler):
@@ -12,14 +13,16 @@ def test_managed_allocations(scheduler):
 
     scheduler.commit()
 
-    # # create a second scheduler with the same context
-    # import ipdb; ipdb.set_trace()
-    # scheduler = database.get_new_scheduler()
+    # create a second scheduler using the same context
+    s2 = new_scheduler(
+        scheduler.context.name,
+        'second scheduler'
+    )
 
-    # allocations = s2.allocate((start, end), timezone)
-    # assert len(allocations) == 1
+    allocations = s2.allocate((start, end), timezone)
+    assert len(allocations) == 1
 
-    # s2.commit()
+    s2.commit()
 
-    # assert s1.managed_allocations().count() == 1
-    # assert s2.managed_allocations().count() == 1
+    assert scheduler.managed_allocations().count() == 1
+    assert s2.managed_allocations().count() == 1
