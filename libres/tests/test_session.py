2b07234b8bd8b80ab11633238b95798f41a9ae62
Denis Krienbühl <denis@href.ch>
2014-11-28 16:46:55 +0100

    Remove non-collision test for now

--- libres/tests/test_session.py
+++ libres/tests/test_session.py
@@ -37,7 +37,8 @@ class ExceptionThread(Thread):
             self.call()
             import time
             time.sleep(1)
-            self.commit()
+            if self.commit is not None:
+                self.commit()
         except Exception as e:
             self.exception = e
 
@@ -100,18 +101,24 @@ def test_sessionstore(dsn):
     assert t1.serial_id != t2.serial_id
 
 
-def test_collission(scheduler):
+def test_collision(scheduler):
 
     scheduler.allocate(
         (datetime(2014, 11, 27, 12), datetime(2014, 11, 27, 13))
     )
+    scheduler.commit()
 
-    def change_allocation():
+    @serialized
+    def change_allocation(scheduler):
         a = scheduler.session.query(Allocation).one()
         a.group = new_uuid()
 
-    t1 = ExceptionThread(serialized(change_allocation))
-    t2 = ExceptionThread(serialized(change_allocation))
+    t1 = ExceptionThread(
+        lambda: change_allocation(scheduler), scheduler.commit
+    )
+    t2 = ExceptionThread(
+        lambda: change_allocation(scheduler), scheduler.commit
+    )
 
     t1.start()
     t2.start()
@@ -126,47 +133,8 @@ def test_collission(scheduler):
     )
     is_nothing = lambda ex: not is_rollback(ex)
 
-    rollbacks = filter(is_rollback, exceptions)
-    updates = filter(is_nothing, exceptions)
-
-    assert len(rollbacks) == 1
-    assert len(updates) == 1
-
-
-def test_non_collission(scheduler):
-    # same as above, but this time the second session is read only and
-    # should therefore not have an impact on existing code
-
-    scheduler.allocate(
-        (datetime(2014, 11, 27, 12), datetime(2014, 11, 27, 13))
-    )
-
-    def change_allocation():
-        a = scheduler.session.query(Allocation).one()
-        a.group = new_uuid()
-
-    def read_allocation():
-        allocation = scheduler.session.query(Allocation).one()
-        allocation.resource
-
-    t1 = ExceptionThread(serialized(change_allocation))
-    t2 = ExceptionThread(serialized(change_allocation))
-
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
-
-    exceptions = (t1.exception, t2.exception)
-
-    is_rollback = lambda ex: ex and isinstance(
-        ex.orig, TransactionRollbackError
-    )
-    is_nothing = lambda ex: not is_rollback(ex)
-
-    rollbacks = filter(is_rollback, exceptions)
-    updates = filter(is_nothing, exceptions)
+    rollbacks = list(filter(is_rollback, exceptions))
+    updates = list(filter(is_nothing, exceptions))
 
     assert len(rollbacks) == 1
     assert len(updates) == 1
