570172768121aeea0bb3cc610f3daf398da1dd91
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-03-16 10:33:29 +0100

    Prepare next release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_long_description():
 
 setup(
     name='libres',
-    version='0.0.2',
+    version='0.0.3',
     url='http://github.com/seantis/libres/',
     license='BSD',
     author='Denis Krienbühl',
