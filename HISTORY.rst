3a0eae5c2f61b6aacc6e48ac0ec9c2ec6f4cd690
Denis Krienbühl <denis.krienbuehl@seantis.ch>
2015-07-30 11:48:31 +0200

    Release 0.1.0

--- HISTORY.rst
+++ HISTORY.rst
@@ -1,8 +1,8 @@
 Changelog
 ---------
 
-0.1.0 (unreleased)
-~~~~~~~~~~~~~~~~~~
+0.1.0 (2015-7-30)
+~~~~~~~~~~~~~~~~~
 
 - BREAKING CHANGE: This release switches to a single SERIALIZED connections.
   
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def get_long_description():
 
 setup(
     name='libres',
-    version='0.0.3',
+    version='0.1.0',
     url='http://github.com/seantis/libres/',
     license='BSD',
     author='Denis Krienbühl',
